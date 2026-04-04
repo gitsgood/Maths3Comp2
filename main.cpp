0d6a2192d56248b444162441edf84af8c767c2db
gitsgood <christoforoskhouri@gmail.com>
2026-04-04 22:38:00 +0200

    tests

--- main.cpp
+++ main.cpp
@@ -2,31 +2,36 @@
 
 #define stringify(x) #x
 
+static_assert(sizeof(Matrixes<int>) == sizeof(Matrixes<double>), "Matrixes struct should have the same size regardless of the type of its elements.");
+static_assert(sizeof(Matrixes<int>) == sizeof(std::vector<std::vector<int>>) + 2 * sizeof(size_t), "Matrixes struct should only contain a vector of vectors and two size_t variables.");
+static_assert(std::is_same_v<decltype(std::declval<Matrixes<int>>() * 1.0), Matrixes<double>>, "Multiplying Matrixes<int> by double should produce Matrixes<double>");
+
 int main()
 {
-	Matrixes<int> test({ {1, 2, 3}, 
-						{4, 5, 6}, 
-						{7, 8, 9} });
+	//Matrixes<int> test({ {1, 2, 3}, 
+	//					{4, 5, 6}, 
+	//					{7, 8, 9} });
 
-	Matrixes<double> test2({ {1, 2, 3},
-							 {4, 5, 6},
-							 {7, 8, 9} });
+	//Matrixes<double> test2({ {1, 2, 3},
+	//						 {4, 5, 6},
+	//						 {7, 8, 9} });
 
-	std::cout << test.GetColumnSize() << "\n" << test.GetRowSize() << "\n";
-	if (test.IsMatrixSquare())
-		std::cout << stringify(test) << " is square\n";
-	else
-		std::cout << stringify(test) << " is NOT square\n";
-	std::cout << test.GetNumberAt(3, 3) << std::endl;
+	//std::cout << test.GetColumnSize() << "\n" << test.GetRowSize() << "\n";
+	//if (test.IsMatrixSquare())
+	//	std::cout << stringify(test) << " is square\n";
+	//else
+	//	std::cout << stringify(test) << " is NOT square\n";
+	//std::cout << test.GetNumberAt(3, 3) << std::endl;
 
-	test.PrintMatrix();
+	//test.PrintMatrix();
 
-	test.GetTranspose().PrintMatrix();
+	//test.GetTranspose().PrintMatrix();
 
-	(test * test.GetTranspose()).PrintMatrix();
+	//(test * test.GetTranspose()).PrintMatrix();
 
-	(test * (int)1.5).PrintMatrix();
+	//(test * (int)1.5).PrintMatrix();
 
-	(test + test2).PrintMatrix();
+	//(test + test2).PrintMatrix();
 
+	return 0;
 }
